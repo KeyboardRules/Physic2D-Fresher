d2e2dadabe487832ab6fcc812842cb399b25ca30
DESKTOP-U0NCVP0\MrTung2 <tungnv@monsterstudio.io>
2021-04-09 17:51:53 +0700

    Add project files.

---
+++ Assets/Boom.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boom : MonoBehaviour
+{
+    public GameObject boom;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject explosion = Instantiate(boom, transform);
+            Destroy(explosion, 0.1f);
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject prefabs;
+    public float timeBetweenSpawn;
+    public float timeExist;
+    float timeCountdown;
+
+    void Update()
+    {
+        if (timeCountdown >= timeBetweenSpawn)
+        {
+            timeCountdown = 0f;
+            GameObject objs= (GameObject)Instantiate(prefabs, transform.position, Quaternion.identity);
+            Destroy(objs, timeExist);
+        }
+        timeCountdown += Time.deltaTime;
+    }
+}
